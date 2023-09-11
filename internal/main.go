63316b06acd8e0cdeaac5b6ca6534b53343a8561
Reticent Facade <lipisharma2911@gmail.com>
2023-09-11 23:25:54 +0530

    initial commit

--- internal/main.go
+++ internal/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -35,39 +36,46 @@ func main() {
 	data.Add("password", password)
 	req, err := http.NewRequest("POST", loginURL, strings.NewReader(data.Encode()))
 	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
+		log.Println("Error creating request:", err)
 	}
+	log.Println(`
+	POST request created:
+	URL:`, req.URL, `
+	Body:`, req.Body, `
+	Header:`, req.Header, `
+
+	Username: `, username, `
+	Password: `, password, `
+	`)
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+
+	log.Println("Sending POST request...")
+	log.Println("Request:", req)
 
 	// Login request:
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error logging in:", err)
-		return
+		log.Println("Error sending request:", err)
 	}
 	defer resp.Body.Close()
 
 	// Checking HTTP status code (successful response = (status code) 200 OK):
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Login failed. Status code:", resp.Status)
-		return
+		log.Println("Error: login failed. Status code:", resp.StatusCode)
 	}
 
 	// Parse HTML of logged-in page (using goquery):
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
-		fmt.Println("Error parsing logged-in page:", err)
-		return
+		log.Println("Error parsing HTML:", err)
 	}
 
 	// Does any specific element indicate success?
 	if doc.Find("#logged-in-indicator").Length() == 0 {
-		fmt.Println("Login failed. Missing indicator element.")
-		return
+		log.Println("Error: login failed. No logged-in indicator found.")
 	}
 
 	// Login successful:
-	fmt.Println("Login successful!")
+	log.Println("Login successful!")
 }
